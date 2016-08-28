11df3b7a487aaa9a7f077aa67596c72ae1478895
Tam Le <lemt93@gmail.com>
2016-08-28 15:15:08 +0700

    Add  mock and proxy examples

---
+++ server/mocks/frameworks.js
@@ -0,0 +1,47 @@
+/*jshint node:true*/
+module.exports = function(app) {
+  var express = require('express');
+  var frameworksRouter = express.Router();
+
+  frameworksRouter.get('/', function(req, res) {
+    res.send({
+      'frameworks': []
+    });
+  });
+
+  frameworksRouter.post('/', function(req, res) {
+    res.status(201).end();
+  });
+
+  frameworksRouter.get('/:id', function(req, res) {
+    res.send({
+      'frameworks': {
+        id: req.params.id
+      }
+    });
+  });
+
+  frameworksRouter.put('/:id', function(req, res) {
+    res.send({
+      'frameworks': {
+        id: req.params.id
+      }
+    });
+  });
+
+  frameworksRouter.delete('/:id', function(req, res) {
+    res.status(204).end();
+  });
+
+  // The POST and PUT call will not contain a request body
+  // because the body-parser is not included by default.
+  // To use req.body, run:
+
+  //    npm install --save-dev body-parser
+
+  // After installing, you need to `use` the body-parser for
+  // this mock uncommenting the following line:
+  //
+  //app.use('/api/frameworks', require('body-parser').json());
+  app.use('/api/frameworks', frameworksRouter);
+};

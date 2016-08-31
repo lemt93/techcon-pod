208a8b60d97058f9c47ee2615d9ead5dc69d6ca1
Tam Le <lemt93@gmail.com>
2016-08-31 19:58:07 +0700

    async value is true by default. Don't need explicit declaration

--- app/models/framework.js
+++ app/models/framework.js
@@ -3,5 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   logo: DS.attr('string'),
-  dependencies: DS.hasMany('dependency', { async: true })
+  dependencies: DS.hasMany('dependency')
 });
